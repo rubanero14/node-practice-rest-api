1715fc6150a647d1e35a0d0134d16fd16e309f45
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-30 15:04:34 +0800

    updated codebase and refactoring some codes

--- view-components/error.js
+++ view-components/error.js
@@ -1,12 +1,12 @@
 const errorView = `
-<style>
-    span {
-        display: flex;
-    }
-    pre {
-        margin: 0 5px;
-        color: red;
-    }
+    <style>
+        span {
+            display: flex;
+        }
+        pre {
+            margin: 0 5px;
+            color: red;
+        }
     </style>
     <span>Hey send some req queries on the url, ie: <pre>?body=ipsum</pre> or <pre>?postId=3</pre> to filter or search comments with word ipsum or postId or other variables</span>
     <br />
