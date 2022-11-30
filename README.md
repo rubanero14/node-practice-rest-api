2dbb9b10d627de1617098088cf02e28ad81587eb
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-30 17:13:35 +0800

    updated docs

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # node-practice-rest-api
+
 Demo Web-Service: https://node-practice-rest-api.onrender.com/
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
-npm start
+npm run dev
 ```
--- view-components/index.js
+++ view-components/index.js
@@ -5,21 +5,45 @@ const homeView = `
             display: block;
             margin: 0 5px;
         }
+
         samp, a {
             font-size: 13px;
         }
+
+        button {
+            width: fit-content;
+            margin: 3px 5px;
+        }
+
         button a {
             font-size: 13px;
             border-radius: 4px;
         }
+
         .container {
             width: auto;
+            padding: 30px;
         }
+
+        .source-code {
+            width: 200px;
+            font-size: 20px;
+        }
+
         @media (max-width: 768px) {
             button a {
                 font-size: 20px;
             }
+
             .container {
+                width: fit-content;
+            }
+
+            .source-code {
+                width: 100%;
+            }    
+
+            button {
                 width: 100%;
             }
         }
@@ -30,15 +54,22 @@ const homeView = `
         <ul>
             <li><kbd>GET Top Post</kbd> => <samp>[method: <em>GET</em>]</samp> => <button><a target="_blank" href="/top-posts">/top-posts<a></button></li>
             <br/>
-            <li><kbd>GET Search or Filter for Comments</kbd> => <samp>This endpoint uses req.query, filter using id, postId, body, name, or email</samp> [method: <em>GET</em>] => <button><a target="_blank" href="/filtered-comments?body=ipsum">/filtered-comments?body=ipsum<a></button></li>
+            <li><kbd>GET Search or Filter for Comments</kbd> => <samp>This endpoint uses req.query, filter using id, postId, body, name, or email</samp> [method: <em>GET</em>] => 
+                <button><a target="_blank" href="/filtered-comments?postId=1">/filtered-comments?postId=1</a></button>
+                <button><a target="_blank" href="/filtered-comments?id=1">/filtered-comments?id=1</a></button>
+                <button><a target="_blank" href="/filtered-comments?name=lorem">/filtered-comments?name=lorem</a></button>
+                <button><a target="_blank" href="/filtered-comments?email=Nikita">/filtered-comments?email=Nikita</a></button>
+                <button><a target="_blank" href="/filtered-comments?body=ipsum">/filtered-comments?body=ipsum</a></button>
+            </li>
             <br/>
-            <li><kbd>GET All Comments</kbd> => [method: <em>GET</em>] => <button><a target="_blank" href="/comments">/comments<a></button></li>
+            <li><kbd>GET All Comments</kbd> => <samp>[method: <em>GET</em>]</samp> => <button><a target="_blank" href="/comments">/comments<a></button></li>
             <br/>
-            <li><kbd>GET All Posts</kbd> => [method: <em>GET</em>] => <button><a target="_blank" href="/posts">/posts<a></button></li>
+            <li><kbd>GET All Posts</kbd> => <samp>[method: <em>GET</em>]</samp> => <button><a target="_blank" href="/posts">/posts<a></button></li>
             <br/>
             <li><kbd>GET Post with Specific ID</kbd> => <samp>where :id type is int</samp> [method: <em>GET</em>] => <button><a target="_blank" href="/posts">/posts/:id<a></button></li>
         </ul>
-        <button><a target="_blank" href="https://github.com/rubanero14/node-practice-rest-api">Source Code<a></button>
+        <br/>
+        <button class="source-code"><a target="_blank" href="https://github.com/rubanero14/node-practice-rest-api">Source Code<a></button>
     </div>
 `;
 
