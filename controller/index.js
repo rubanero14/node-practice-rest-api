d8dea5e1bc93f3f2df864386bb86f1166610f6c2
Rajaruban Rajindram <ahyat888@gmail.com>
2022-11-30 15:27:30 +0800

    nested controller logic inside try/catch method for proper error handling

--- controller/index.js
+++ controller/index.js
@@ -5,127 +5,155 @@ const homeView = require("../view-components");
 const errorView = require("../view-components/error");
 
 exports.getTopPosts = async (req, res, next) => {
-  const comments = await axios.get(`${Constants.BASE_URL}comments`, {
-    headers: Constants.headerJsonConfig,
-  });
-
-  const posts = await axios.get(`${Constants.BASE_URL}posts`, {
-    headers: Constants.headerJsonConfig,
-  });
-
-  const topPost = [];
-  let numberOfComments = 0;
-
-  posts.data.map((post) => {
-    comments.data.map((comment) => {
-      if (post.id === comment.postId) {
-        numberOfComments++;
-        topPost.push({
-          post_id: post.id,
-          post_title: post.title,
-          post_body: post.body,
-          total_number_of_comments: numberOfComments,
-        });
-      } else {
-        numberOfComments = 0;
-      }
+  try {
+    const comments = await axios.get(`${Constants.BASE_URL}comments`, {
+      headers: Constants.headerJsonConfig,
     });
-  });
 
-  const sortedPost = topPost.sort(
-    (a, b) =>
-      parseFloat(b.total_number_of_comments) -
-      parseFloat(a.total_number_of_comments)
-  );
+    const posts = await axios.get(`${Constants.BASE_URL}posts`, {
+      headers: Constants.headerJsonConfig,
+    });
 
-  res.send(sortedPost);
+    const topPost = [];
+    let numberOfComments = 0;
+
+    posts.data.map((post) => {
+      comments.data.map((comment) => {
+        if (post.id === comment.postId) {
+          numberOfComments++;
+          topPost.push({
+            post_id: post.id,
+            post_title: post.title,
+            post_body: post.body,
+            total_number_of_comments: numberOfComments,
+          });
+        } else {
+          numberOfComments = 0;
+        }
+      });
+    });
+
+    const sortedPost = topPost.sort(
+      (a, b) =>
+        parseFloat(b.total_number_of_comments) -
+        parseFloat(a.total_number_of_comments)
+    );
+
+    res.send(sortedPost);
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
 
 exports.getFilteredComment = async (req, res, next) => {
-  await axios
-    .get(`${Constants.BASE_URL}comments`, {
-      headers: Constants.headerJsonConfig,
-    })
-    .then((response) => {
-      const queryKey = Object.keys(req.query)[0];
-      const queryValue = req.query[queryKey];
-      const body = req.query.body;
+  try {
+    await axios
+      .get(`${Constants.BASE_URL}comments`, {
+        headers: Constants.headerJsonConfig,
+      })
+      .then((response) => {
+        const queryKey = Object.keys(req.query)[0];
+        const queryValue = req.query[queryKey];
+        const body = req.query.body;
 
-      const comments = response.data;
-      const results = [];
-      let numberOfCommentsFound = 0;
-      let data = "";
+        const comments = response.data;
+        const results = [];
+        let numberOfCommentsFound = 0;
+        let data = "";
 
-      const filterComments = (key, value) => {
-        for (const comment of comments) {
-          if (body) {
-            if (comment["body"].includes(body)) {
-              results.push(comment["body"]);
-              numberOfCommentsFound++;
+        const filterComments = (key, value) => {
+          for (const comment of comments) {
+            if (body) {
+              if (comment["body"].includes(body)) {
+                results.push(comment["body"]);
+                numberOfCommentsFound++;
+              }
             }
-          }
 
-          if (value) {
-            if (comment[key] == value) {
-              results.push(comment["body"]);
-              numberOfCommentsFound++;
+            if (value) {
+              if (comment[key] == value) {
+                results.push(comment["body"]);
+                numberOfCommentsFound++;
+              }
             }
+
+            if (key === undefined || value === undefined) {
+              res.send(errorView);
+            }
+
+            data = {
+              comments_found_array: results,
+              number_of_posts_found: numberOfCommentsFound,
+            };
           }
+        };
 
-          if (key === undefined || value === undefined) {
-            res.send(errorView);
-          }
+        filterComments(queryKey, queryValue);
 
-          data = {
-            comments_found_array: results,
-            number_of_posts_found: numberOfCommentsFound,
-          };
-        }
-      };
-
-      filterComments(queryKey, queryValue);
-
-      res.send(data);
-    })
-    .catch((err) => console.log(err));
+        res.send(data);
+      })
+      .catch((err) => console.log(err));
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
 
 exports.getAllComments = async (req, res, next) => {
-  const response = await axios
-    .get(`${Constants.BASE_URL}comments`, {
-      headers: Constants.headerJsonConfig,
-    })
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
-  res.send(response);
+  try {
+    const response = await axios
+      .get(`${Constants.BASE_URL}comments`, {
+        headers: Constants.headerJsonConfig,
+      })
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
+    res.send(response);
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
 
 exports.getAllPosts = async (req, res, next) => {
-  const response = await axios
-    .get(`${Constants.BASE_URL}posts`, {
-      headers: Constants.headerJsonConfig,
-    })
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
-  res.send(response);
+  try {
+    const response = await axios
+      .get(`${Constants.BASE_URL}posts`, {
+        headers: Constants.headerJsonConfig,
+      })
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
+    res.send(response);
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
 
 exports.getSpecificPost = async (req, res, next) => {
-  const id = req.params.id;
-  const response = await axios
-    .get(`${Constants.BASE_URL}posts/${id}`, {
-      headers: Constants.headerJsonConfig,
-    })
-    .then((response) => response.data)
-    .catch((err) => console.log(err));
-  res.send(response);
+  try {
+    const id = req.params.id;
+    const response = await axios
+      .get(`${Constants.BASE_URL}posts/${id}`, {
+        headers: Constants.headerJsonConfig,
+      })
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
+    res.send(response);
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
 
 exports.getDocumentation = async (req, res, next) => {
-  // Example of simple endpoint documentation
-  res.send(homeView);
+  try {
+    // Example of simple endpoint documentation
+    res.send(homeView);
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
 
 exports.pageNotFound = (req, res) => {
-  res.status(404).send("404: Page not found");
+  try {
+    res.status(404).send("404: Page not found");
+  } catch (err) {
+    res.status(503).send(err);
+  }
 };
